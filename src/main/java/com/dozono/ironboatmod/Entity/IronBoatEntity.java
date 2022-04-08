4e5bedf2db9286da8136a6c12aa11ee22ee51909
dozono <chrislee.hao@gmail.com>
2022-04-08 14:40:44 +0800

    init

--- src/main/java/com/dozono/ironboatmod/Entity/IronBoatEntity.java
+++ src/main/java/com/dozono/ironboatmod/Entity/IronBoatEntity.java
@@ -1,2 +1,14 @@
-package com.dozono.ironboatmod.Entity;public class IronBoatEntity {
+package com.dozono.ironboatmod.Entity;
+
+import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.level.Level;
+
+public class IronBoatEntity extends Boat {
+    public IronBoatEntity(Level p_38293_, double p_38294_, double p_38295_, double p_38296_) {
+        super(p_38293_, p_38294_, p_38295_, p_38296_);
+
+    }
+
+
+
 }
--- src/main/java/com/dozono/ironboatmod/IronBoatMod.java
+++ src/main/java/com/dozono/ironboatmod/IronBoatMod.java
@@ -1,10 +1,14 @@
-package com.example.examplemod;
+package com.dozono.ironboatmod;
 
+import com.dozono.ironboatmod.Entity.IronBoatEntity;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -13,19 +17,26 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 import java.util.stream.Collectors;
 
+import static com.dozono.ironboatmod.IronBoatMod.MODID;
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(MODID)
-public class ExampleMod
+public class IronBoatMod
 {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     public static final String MODID = "iron_boat";
+    private static final DeferredRegister<EntityType<?>> ENTITY = DeferredRegister.create(ForgeRegistries.ENTITIES,MODID);
+//    public static final RegistryObject<EntityType<IronBoatEntity>> IRON_BOAT_ENTITY = re
 
-    public ExampleMod()
+    public IronBoatMod()
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
@@ -36,6 +47,8 @@ public class ExampleMod
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event)
